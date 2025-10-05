d3cd1541f5ceb62ade595a88e6ade90ad7237835
abyplr <abyplr@ciencias.unam.mx>
2025-10-05 13:46:28 -0600

    Delete Ejercicio3.py

--- Ejercicio3.py
+++
@@ -1,6 +0,0 @@
-pi = float(input("Ingrese el precio del producto: "))
-des = float(input("Ingrese el porcentaje de descuento: "))
-
-pf = pi - ((des * pi) / 100)
-
-print("El precio final del producto, con el descuento aplicado es: ", pf)
